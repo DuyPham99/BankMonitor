8d1ac28e4352cdba83bf51ddd444fbcf758a1bc2
DUY <pducduy99@gmail.com>
2020-10-13 00:33:53 +0700

    Add project files.

---
+++ BankMonitor/views/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BankMonitor
+{
+    public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bbtnpnStaff_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            if (this.pnStaff.Visible != true) HidePanels(this.Controls);
+            this.pnStaff.Visible = !this.pnStaff.Visible;
+        }
+        public void HidePanels(Control.ControlCollection controls)
+        {
+            foreach (Control c in controls)
+            {
+                if (c is Panel)
+                {
+                    c.Visible = false;
+                }
+
+                // hide any panels this control may have
+                HidePanels(c.Controls);
+            }
+        } 
+        private void bbtnCustomer_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            if(this.pnCustomer.Visible != true) HidePanels(this.Controls);
+            this.pnCustomer.Visible = !this.pnCustomer.Visible;
+        }
+
+       
+    }
+}
